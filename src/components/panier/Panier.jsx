b622f04549833a79a18dcf671aca8bf6a7f8058b
Emil <somaleyoo123@gmail.com>
2025-06-18 15:51:30 +0200

    merge

---
+++ src/components/panier/Panier.jsx
@@ -0,0 +1,15 @@
+import './Panier.css'
+
+export default function Panier() {
+    
+
+
+    return(
+        <>
+            <div className="panier-container">
+                <h2>Panier d'achat</h2>
+                <div className=""></div>
+            </div>
+        </>
+    )
+}
