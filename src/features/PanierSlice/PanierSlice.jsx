ba96a69c4813d81cccc2cf144bfec86b654905af
Kamil Baldyga <baldygakamil@gmail.com>
2025-06-18 14:00:40 +0200

    add slices

---
+++ src/features/PanierSlice/PanierSlice.jsx
@@ -0,0 +1,10 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {};
+
+export const PanierSlice = createSlice({
+  name: "panier",
+  initialState: initialState,
+
+  reducers: {},
+});
---
+++ src/features/PizzaSlice/PizzaSlice.jsx
@@ -0,0 +1,10 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {};
+
+export const PizzaSlice = createSlice({
+  name: "pizza",
+  initialState: initialState,
+
+  reducers: {},
+});
