7e58aaed1237e5ba0adfd92c1787d5d8694f58cb
Emil <somaleyoo123@gmail.com>
2025-06-18 15:21:50 +0200

    style navbar

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,24 @@
 import Navbar from "../components/navbar/Navbar";
-
+import data from '../data.json'
 export default function Home() {
     
 
     return(
         <>
             <Navbar/>
+            <section id="pizza">
+                <div className="container">
+                    <div className="titre">
+                        <hr /><p>Pizza</p><hr />
+                    </div>
+                    <div className="card-container">
+                       
+                    </div>
+                </div>
+                
+            </section>
+            
+            
         </>
     )
 }
