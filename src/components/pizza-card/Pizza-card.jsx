be462d49e668c013c215f1a3577309ebf4a73a31
Emil <somaleyoo123@gmail.com>
2025-06-18 16:43:25 +0200

    pizza container style

--- src/components/pizza-card/Pizza-card.jsx
+++ src/components/pizza-card/Pizza-card.jsx
@@ -7,14 +7,14 @@ export default function PizzaCard() {
     <div className="pizza-container">
       {data.map((pizza, index) => (
         <div className="card" key={index}>
-          <img src={pizza.image} alt={pizza.nom} />
+          <img className="pizza-img" src={pizza.image} alt={pizza.nom} />
           <div className="card-body">
             <h5 className="card-title">{pizza.nom}</h5>
             <p className="card-text">{pizza.description}</p>
           </div>
           <div className="card-footer">
             <p className="card-prix">
-              à partir de <span>€{pizza.prix}</span>
+              à partir de <span className="prix">€{pizza.prix}</span>
             </p>
             <a href="#">+</a>
           </div>
