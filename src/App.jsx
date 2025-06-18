74298f9af4cda9fde2b9b0c0ddba1623b1c97d80
Emil <somaleyoo123@gmail.com>
2025-06-18 14:31:32 +0200

    setup navbar home

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,16 @@
+import { Route, Routes } from "react-router-dom";
 import "./App.css";
+import Home from "./pages/Home";
 
 function App() {
   return (
     <>
       {/* Ready to code! */}
       {/* Script written by github.com/ME0WGE */}
+      
+      <Routes>
+        <Route path="/" element={<Home/>}/>
+      </Routes>
     </>
   );
 }
---
+++ src/components/navbar/Navbar.jsx
@@ -0,0 +1,13 @@
+import './Nav.css'
+export default function Navbar() {
+    
+
+
+    return(
+        <>
+            <nav>
+                text test est
+            </nav>
+        </>
+    )
+}
