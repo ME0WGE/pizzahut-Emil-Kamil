f5e69a487333c6956a43853d6a6e58152c9d7df3
Kamil Baldyga <baldygakamil@gmail.com>
2025-06-19 10:27:22 +0200

    add thankyou page style & structure

--- src/pages/Thankyou.jsx
+++ src/pages/Thankyou.jsx
@@ -1,3 +1,28 @@
+import { Link } from "react-router-dom";
+import Navbar from "../components/navbar/Navbar";
+import Footer from "../components/footer/Footer";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCheckCircle } from "@fortawesome/free-solid-svg-icons";
+import "./Thankyou.css";
+
 export default function Thankyou() {
-  return;
+  return (
+    <>
+      <Navbar />
+      <div className="thankyou-container">
+        <div className="thankyou-card">
+          <div className="check-circle">
+            <FontAwesomeIcon icon={faCheckCircle} className="check-icon" />
+          </div>
+          <h2>Commande Confirmée</h2>
+          <p>Votre commande sera livrée dans les plus brefs délais</p>
+          <p className="invoice-link">Voir votre facture</p>
+          <Link to="/" className="go-home-btn">
+            Retourner à l'accueil
+          </Link>
+        </div>
+      </div>
+      <Footer />
+    </>
+  );
 }
