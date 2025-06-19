d8abdf4872f78140605afa91dcede00049d81dd4
Kamil Baldyga <baldygakamil@gmail.com>
2025-06-19 11:38:47 +0200

    assemble code for functionnal panier

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,36 +1,9 @@
-import { useDispatch, useSelector } from "react-redux";
 import Footer from "../components/footer/Footer";
 import Navbar from "../components/navbar/Navbar";
 import Panier from "../components/panier/Panier";
 import PizzaCard from "../components/pizza-card/Pizza-card";
-import {
-  removeFromPanier,
-  increaseQuantity,
-  decreaseQuantity,
-} from "../features/PanierSlice/PanierSlice";
 
 export default function Home() {
-  const panier = useSelector((state) => state.panier);
-  const dispatch = useDispatch();
-
-  const handleRemoveItem = (index) => {
-    dispatch(removeFromPanier(index));
-  };
-
-  const handleIncreaseQuantity = (index) => {
-    dispatch(increaseQuantity(index));
-  };
-
-  const handleDecreaseQuantity = (index) => {
-    dispatch(decreaseQuantity(index));
-  };
-
-  const calculateTotal = () => {
-    return panier
-      .reduce((total, item) => total + item.prix * item.quantite, 0)
-      .toFixed(2);
-  };
-
   return (
     <>
       <Navbar />
