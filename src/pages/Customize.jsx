1536d08782eef300c18a825219dcd0f9754ef241
Kamil Baldyga <baldygakamil@gmail.com>
2025-06-20 11:01:44 +0200

    homepage almost ok

--- src/pages/Customize.jsx
+++ src/pages/Customize.jsx
@@ -1,38 +1,30 @@
-import { useParams } from "react-router-dom"
-import data from '../data.json'
-import "./Customize.css"
-import Navbar from "../components/navbar/Navbar"
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
-import { faChevronLeft } from "@fortawesome/free-solid-svg-icons"
+import { useParams } from "react-router-dom";
+import data from "../data.json";
+import "./Customize.css";
+import Navbar from "../components/navbar/Navbar";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faChevronLeft } from "@fortawesome/free-solid-svg-icons";
 
 export default function Customize() {
-const {id}=useParams()
-const pizza = data.find((item)=>item.nom ===id)
-console.log(pizza);
+  const { id } = useParams();
+  const pizza = data.find((item) => item.nom === id);
+  console.log(pizza);
 
-
-
-return(
+  return (
     <>
-    <Navbar/>
-    <section id="customize">
-      <div className="retour">
-        <FontAwesomeIcon icon={faChevronLeft} />
-        <p>Retour</p>
-      
-      </div>
-       
-          <div   className="customize-container">
-           <div className="customize-image">
-              <img src={pizza.image} alt="" />
-           </div>
+      <Navbar />
+      <section id="customize">
+        <div className="retour">
+          <FontAwesomeIcon icon={faChevronLeft} />
+          <p>Retour</p>
+        </div>
+
+        <div className="customize-container">
+          <div className="customize-image">
+            <img src={pizza.image} alt="" />
           </div>
-    
-        
-  
-     
+        </div>
       </section>
-      
     </>
-  )
+  );
 }
