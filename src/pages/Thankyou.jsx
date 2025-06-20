c20b417c8c7d7a7e55e9ca27a70a53b661e3879e
Kamil Baldyga <baldygakamil@gmail.com>
2025-06-20 15:00:43 +0200

    clearPanier when clicked on retourner à l'accueil

--- src/pages/Thankyou.jsx
+++ src/pages/Thankyou.jsx
@@ -2,8 +2,12 @@ import { Link } from "react-router-dom";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faCheckCircle } from "@fortawesome/free-solid-svg-icons";
 import "./Thankyou.css";
+import { useDispatch } from "react-redux";
+import { clearPanier } from "../features/PanierSlice/PanierSlice";
 
 export default function Thankyou() {
+  const dispatch = useDispatch();
+
   return (
     <>
       <div className="thankyou-container">
@@ -14,7 +18,10 @@ export default function Thankyou() {
           <h2>Commande Confirmée</h2>
           <p>Votre commande sera livrée dans les plus brefs délais</p>
           <p className="invoice-link">Voir votre facture</p>
-          <Link to="/" className="go-home-btn">
+          <Link
+            to="/"
+            className="go-home-btn"
+            onClick={() => dispatch(clearPanier())}>
             Retourner à l'accueil
           </Link>
         </div>
